8c19f13e2eecd76362a76b5d2e2a2af255a54f8c
Herman Tandberg Dybing <hermantd@stud.ntnu.no>
2020-05-09 14:20:36 +0200

    Added camera controls

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,10 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 1f;
+    [SerializeField]
+    private float _rotationSpeed = 1f;
+    [SerializeField]
+    private Transform _camera;
     private Rigidbody _rb;
 
     // Start is called before the first frame update
@@ -22,7 +26,10 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _rb.transform.Rotate(new Vector3(0f, 1f, 0f), Input.GetAxis("Mouse X") * _rotationSpeed);
+        _camera.transform.Rotate(new Vector3(1f, 0f, 0f), Input.GetAxis("Mouse Y") * _rotationSpeed);
+
         Vector3 movementVector = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-        _rb.transform.Translate(movementVector * _speed * Time.deltaTime);
+        _rb.transform.Translate(movementVector * _speed * Time.deltaTime, Space.Self);
     }
 }
