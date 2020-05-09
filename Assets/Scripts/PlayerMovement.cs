572a7d62488649cc24bb085db875e1854188ac4b
Herman Tandberg Dybing <hermantd@stud.ntnu.no>
2020-05-09 13:45:34 +0200

    Setup some basic player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 1f;
+    private Rigidbody _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        if (_rb == null)
+        {
+            Debug.LogError("No Rigidbody found, creating a new one");
+            _rb = new Rigidbody();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movementVector = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        _rb.transform.Translate(movementVector * _speed * Time.deltaTime);
+    }
+}
