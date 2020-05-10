120066bfc9e8cb919eb9c2e9db9dc2312819f16b
Herman Tandberg Dybing <hermantd@stud.ntnu.no>
2020-05-10 17:22:55 +0200

    Added pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _menu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_menu.activeInHierarchy == true)
+            {
+                DisableMenu();
+            }
+            else
+            {
+                EnableMenu();
+            }
+        }
+    }
+
+    private void EnableMenu ()
+    {
+        _menu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void DisableMenu ()
+    {
+        _menu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void RestartGame ()
+    {
+        DisableMenu();
+        SceneManager.LoadScene("TimeTrial");
+    }
+
+    public void QuitGame ()
+    {
+        DisableMenu();
+        Application.Quit();
+    }
+}
